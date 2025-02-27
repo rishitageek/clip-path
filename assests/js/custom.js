f7da97a34917b8f5aab0d6f0c38aab1a1832d9c3
rishitageek <rishitapgeek435@gmail.com>
2025-02-27 18:18:09 +0530

    page design

--- assests/js/custom.js
+++ assests/js/custom.js
@@ -7,38 +7,54 @@ function toggleMenu() {
     }
 }
 
-        // slider js
-        $('.cl__gallery_imgwrap').slick({
-            dots: true,
-            infinite: false,
-            speed: 300,
-            rows: 3,
-            slidesToShow: 3,
-            slidesToScroll: 3,
-            arrows: false,
-            responsive: [
-                {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3,
-                    infinite: true,
-                    dots: true
-                }
-                },
-                {
-                breakpoint: 600,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2
-                }
-                },
-                {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-                }
-            ]
-        });
+$('.cl__gallery_imgwrap').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    rows: 3,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    arrows: false,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 991,
+            settings: {
+                rows: 2,
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                rows: 1,
+                slidesToShow: 2, 
+                slidesToScroll: 1,
+                dots: false,
+                arrows: true,
+            }
+        },
+        {
+            breakpoint: 575,
+            settings: {
+                rows: 1,
+                slidesToShow: 1,  
+                slidesToScroll: 1,
+                dots: false,
+                autoplay: true,
+                autoplaySpeed: 2000,
+                arrows: true,
+            }
+        }
+    ]
+});
